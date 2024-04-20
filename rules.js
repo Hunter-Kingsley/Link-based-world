b2a92861e83bf195cb45fc3163329262b53f1451
Hunter-Kingsley <hunterkingsley329@gmail.com>
2024-04-19 21:50:19 -0700

    Added Game Functionality

--- rules.js
+++ rules.js
@@ -1,22 +1,38 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        console.log(this.engine.storyData);
+
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        if (key == "Stove") {
+            this.engine.storyData.Soup_Cooked = true;
+        }
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+
+        console.log(key);
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        if(locationData.Choices) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                if ((this.engine.storyData.Pot_Collected == false) && (choice.Target == "Stove")) {
+                    continue;
+                }
+                if ((this.engine.storyData.Pot_Collected == true) && (choice.Text == "Get out the Pot")) {
+                    continue;
+                }
+                if ((this.engine.storyData.Soup_Cooked == false) && (choice.Text == "Deliver the Soup")) {
+                    continue;
+                }
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
@@ -25,11 +41,41 @@ class Location extends Scene {
     }
 
     handleChoice(choice) {
-        if(choice) {
+        if(choice && choice.Target && !choice.Sub) {
             this.engine.show("&gt; "+choice.Text);
             this.engine.gotoScene(Location, choice.Target);
+        } else if (choice && choice.Target && choice.Sub) {
+            this.engine.show("&gt; "+choice.Text);
+            this.engine.gotoScene(Sub_Location, choice.Target);
         } else {
             this.engine.gotoScene(End);
+        }
+    }
+}
+
+class Sub_Location extends Location {
+    create(key) {
+        if (key == "Cabinets") {
+            this.engine.storyData.Pot_Collected = true;
+        }
+        let locationData = this.engine.storyData.Locations[key]; 
+        let num = Math.floor(Math.random() * locationData.Dialouge.length);
+        //console.log(locationData.Dialouge.length);
+        this.engine.show(locationData.Dialouge[num]);
+        console.log(num);
+        
+        if(locationData.Choices) { 
+            for(let choice of locationData.Choices) {
+                if ((this.engine.storyData.Pot_Collected == true) && (choice.Text == "Get out the Pot")) {
+                    continue;
+                }
+                if ((this.engine.storyData.Soup_Cooked == false) && (choice.Text == "Deliver the Soup")) {
+                    continue;
+                }
+                this.engine.addChoice(choice.Text, choice); 
+            }
+        } else {
+            this.engine.addChoice("The end.")
         }
     }
 }
